899c393f76eac228a0957ea527444a3979125c06
Akshatverma79 <akshatverma90325@gmail.com>
2026-02-03 14:19:20 +0530

    Add examples of object destructuring with student data

---
+++ feb3/Renaming_Variable.js
@@ -0,0 +1,10 @@
+let student={
+    name:"Alice",
+    age:22,
+};
+let{name,age}=student;
+let studentName=student.name;
+let studentAge=student.age;
+console.log(studentAge,studentName);
+// let{name:studentName,age:studentAge}=student;
+// console.log(studentName,studentAge);
---
+++ feb3/object_Destructuring.js
@@ -0,0 +1,7 @@
+let student={
+    name:"Alice",
+    age:22,
+    course:"Mathematics"
+};
+let {name,age,course}=student;
+console.log(name,age,course);

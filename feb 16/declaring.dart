7951c6f77562cc435ff7f21cfcd48018d2680ac9
Akshatverma79 <akshatverma90325@gmail.com>
2026-02-16 11:53:38 +0530

    Add examples demonstrating variable declaration and string concatenation in Dart

---
+++ feb 16/declaring.dart
@@ -0,0 +1,8 @@
+void main(){
+  int num1=2;
+  double num2=3.5;
+  print("$num1");
+  print("$num2");
+  var sum=num1+num2;
+  print("Sum of num1 and num2 is $sum");
+}
---
+++ feb 16/string.dart
@@ -0,0 +1,7 @@
+void main(){
+  String string="Web Tech";
+  String str='Coding is';
+  String str1='Fun';
+  print(string);
+  print(str+str1);
+}
